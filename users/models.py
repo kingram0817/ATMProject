945886a517aef250e94313a07bc4ee2bd33f2095
khoradan <khoradan@gmail.com>
2019-11-12 21:24:50 -0600

    updated models.py and rebuilt db

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    accountNumber = models.CharField(max_length=16, primary_key=True)
+    accountNumber = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, default='')
@@ -11,16 +11,16 @@ class CustomUser(AbstractUser):
     address = models.CharField(max_length=50, default='')
     city = models.CharField(max_length=50, default='')
     state = models.CharField(max_length=2, default='')
-    zipCode = models.CharField(max_length=5, default='12345')
-    phoneNumber = models.CharField(max_length=10, default='123456789')
+    zipCode = models.CharField(max_length=5, default='')
+    phoneNumber = models.CharField(max_length=10, default='1112223333')
 
     def __str__(self):
-        return self.last_name + " , " + self.first_name + " || " + str(self.accountNumber)
+        return str(self.accountNumber)
 
 
 class Transaction(models.Model):
-    transactionId = models.CharField(max_length=9999999999999, primary_key=True)
-    atmCardNumber = models.ForeignKey('ATM_Card', on_delete=models.CASCADE)
+    transactionId = models.AutoField(primary_key=True)
+    ATMCardNumber = models.ForeignKey('ATM_Card', on_delete=models.CASCADE)
     date = models.DateField(max_length=50, default='')
     ATMachineUID = models.ForeignKey('ATMachine', on_delete=models.CASCADE)
     status = models.CharField(max_length=50, default='')
@@ -29,20 +29,22 @@ class Transaction(models.Model):
 
 
 class ATM_Card(models.Model):
-    atmCardNumber = models.CharField(max_length=50, primary_key=True)
-    accountNumber = models.ForeignKey('AccountExtension', on_delete=models.CASCADE)
-    pin = models.CharField(max_length=50, default='')
+    atmCardNumber = models.AutoField(primary_key=True)
+    accountNumber = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    pin = models.CharField(max_length=4, default='')
     name = models.CharField(max_length=50, default='')
+    # TODO:Change to DateField
     dateOfIssue = models.CharField(max_length=50, default='')
+    # TODO:Change to DateField
     expirationDate = models.CharField(max_length=50, default='')
     address = models.CharField(max_length=50, default='')
-    phoneNumber = models.CharField(max_length=50, default='')
-    cardStatus = models.CharField(max_length=50, default='')
+    phoneNumber = models.CharField(max_length=10, default='1112223333')
+    cardStatus = models.BooleanField(default=True)
     twoFactorAuthenticationStatus = models.BooleanField(default=True)
 
 
 class PhoneChange(models.Model):
-    newPhoneNumber = models.CharField(max_length=12, default='')
+    newPhoneNumber = models.CharField(max_length=10, default='1112223333')
 
 
 class PinChange(models.Model):
@@ -51,42 +53,43 @@ class PinChange(models.Model):
 
 
 class CashWithdrawal(models.Model):
-    amountTransferred = models.CharField(max_length=999999999999, default='')
+    amountTransferred = models.PositiveIntegerField(default=0)
     denomination = models.CharField(max_length=50, default='')
-    currentBalance = models.CharField(max_length=9999999999999, default='')
+    currentBalance = models.IntegerField(default=0)
 
 
 class CashTransfer(models.Model):
     beneficiaryAccountNumber = models.CharField(max_length=50, default='')
     beneficiaryName = models.CharField(max_length=50, default='')
-    amountTransferred = models.CharField(max_length=999999999999, default='')
+    amountTransferred = models.PositiveIntegerField(default=0)
 
 
 class BalanceEnquiry(models.Model):
-    balanceAmount = models.CharField(max_length=9999999999999, default='')
+    balanceAmount = models.IntegerField(default=0)
 
 
 class ATMachineRefill(models.Model):
-    refillId = models.CharField(max_length=9999999999999, primary_key=True)
+    refillId = models.AutoField(primary_key=True)
     ATMachineUID = models.ForeignKey('ATMachine', on_delete=models.CASCADE)
-    amount = models.CharField(max_length=9999999999999, default='')
-    atmBranch = models.CharField(max_length=9999999999999, default='')
-    refillDate = models.DateField(max_length=9999999999999, default='')
-    previousBalance = models.CharField(max_length=9999999999999, default='')
+    amount = models.PositiveIntegerField(default=0)
+    atmBranch = models.CharField(max_length=50, default='')
+    refillDate = models.DateField(max_length=50, default='')
+    previousBalance = models.PositiveIntegerField(default=0)
 
 
 class ATMachine(models.Model):
-    ATMachineUID = models.CharField(max_length=9999999999999, primary_key=True)
-    currentBalance = models.CharField(max_length=9999999999999, default='')
-    location = models.CharField(max_length=9999999999999, default='')
-    minimumBalance = models.CharField(max_length=9999999999999, default='')
-    status = models.CharField(max_length=9999999999999, default='')
-    lastRefillDate = models.CharField(max_length=9999999999999, default='')
-    nextMaintenanceDate = models.CharField(max_length=9999999999999, default='')
+    ATMachineUID = models.AutoField(primary_key=True)
+    currentBalance = models.PositiveIntegerField(default=0)
+    location = models.CharField(max_length=100, default='')
+    minimumBalance = models.PositiveIntegerField(default=1000)
+    status = models.CharField(max_length=50, default='')
+    lastRefillDate = models.DateField(max_length=50, default='')
+    # TODO:Test nextMaintenanceDate
+    nextMaintenanceDate = models.DateField(max_length=50, default='')
 
 
 class AccountExtension(models.Model):
-    accountNumber = models.CharField(max_length=9999999999999, primary_key=True)
-    name = models.CharField(max_length=9999999999999, default='')
-    phoneNumber = models.CharField(max_length=9999999999999, default='')
-    balance = models.CharField(max_length=9999999999999, default='')
+    accountNumber = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, default='')
+    phoneNumber = models.CharField(max_length=10, default='1112223333')
+    balance = models.IntegerField(default=0)
